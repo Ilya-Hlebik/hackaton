4f7f30b63172532031b04b9b9107553ce90cdbce
Idglebik <felix.2894@mail.ru>
2019-09-28 14:30:44 +0300

    update changeset

--- src/main/java/com/web/assistant/dto/UserResponseDTO.java
+++ src/main/java/com/web/assistant/dto/UserResponseDTO.java
@@ -1,51 +1,22 @@
 package com.web.assistant.dto;
 
-import java.util.List;
-
 import com.web.assistant.model.Role;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
+@Getter
+@Setter
 public class UserResponseDTO {
 
-  @ApiModelProperty(position = 0)
-  private Integer id;
-  @ApiModelProperty(position = 1)
-  private String username;
-  @ApiModelProperty(position = 2)
-  private String email;
-  @ApiModelProperty(position = 3)
-  List<Role> roles;
-
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public List<Role> getRoles() {
-    return roles;
-  }
-
-  public void setRoles(List<Role> roles) {
-    this.roles = roles;
-  }
+    @ApiModelProperty(position = 3)
+    List<Role> roles;
+    @ApiModelProperty(position = 0)
+    private Integer id;
+    @ApiModelProperty(position = 1)
+    private String username;
+    @ApiModelProperty(position = 2)
+    private String email;
 
 }
--- src/main/java/com/web/assistant/model/User.java
+++ src/main/java/com/web/assistant/model/User.java
@@ -1,17 +1,17 @@
 package com.web.assistant.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Size;
 
 @Entity
+@Table(name = "USER")
+@Data
 public class User {
 
   @Id
@@ -28,47 +28,10 @@ public class User {
   @Size(min = 8, message = "Minimum password length: 8 characters")
   private String password;
 
-  @ElementCollection(fetch = FetchType.EAGER)
+  @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+  @CollectionTable(name = "USER_ROLE", joinColumns = @JoinColumn(name = "USER_ID"))
+  @Enumerated(EnumType.STRING)
+  @EqualsAndHashCode.Exclude
+  @ToString.Exclude
   List<Role> roles;
-
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public List<Role> getRoles() {
-    return roles;
-  }
-
-  public void setRoles(List<Role> roles) {
-    this.roles = roles;
-  }
-
 }
