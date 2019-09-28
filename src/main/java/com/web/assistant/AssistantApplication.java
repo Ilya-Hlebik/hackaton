4592548fc5b2d6584d2ecd3af8fcafda3edffddc
Idglebik <felix.2894@mail.ru>
2019-09-28 12:56:46 +0300

    Add security

--- src/main/java/com/web/assistant/AssistantApplication.java
+++ src/main/java/com/web/assistant/AssistantApplication.java
@@ -1,13 +1,22 @@
 package com.web.assistant;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 
 @SpringBootApplication
 public class AssistantApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssistantApplication.class, args);
+	}
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
---
+++ src/main/java/com/web/assistant/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.web.assistant.repository;
+
+import com.web.assistant.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.transaction.Transactional;
+
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    boolean existsByUsername(String username);
+
+    User findByUsername(String username);
+
+    @Transactional
+    void deleteByUsername(String username);
+
+}
