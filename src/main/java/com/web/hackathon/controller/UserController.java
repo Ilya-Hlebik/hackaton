afd26f70fb972dafa8b82189a7a92de49409ec3f
shalukho <alexandr.shalukho@gmail.com>
2020-02-24 11:29:00 +0300

    HC-1 some refactoring, test commit

--- src/main/java/com/web/hackathon/controller/UserController.java
+++ src/main/java/com/web/hackathon/controller/UserController.java
@@ -5,12 +5,22 @@ import com.web.hackathon.dto.ForgotPasswordRequestDto;
 import com.web.hackathon.dto.PasswordUpdateRequestDto;
 import com.web.hackathon.dto.SignInRequestDTO;
 import com.web.hackathon.service.UserService;
-import io.swagger.annotations.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,26 +32,33 @@ import javax.validation.Valid;
 @AllArgsConstructor
 public class UserController {
 
+    public static final String ACCESS_DENIED_RESPONSE_MESSAGE = "Access denied";
+    public static final String USERNAME_IS_ALREADY_IN_USE_RESPONSE_MESSAGE = "Username is already in use";
+    public static final String EXPIRED_OR_INVALID_JWT_TOKEN_MESSAGE = "Expired or invalid JWT token";
+    public static final String SOMETHING_WENT_WRONG_RESPONSE_MESSAGE = "Something went wrong";
+    public static final String YOU_SHOULD_CHANGE_PASSWORD_RESPONSE_MESSAGE = "You should change password";
+    public static final String INVALID_USERNAME_PASSWORD_SUPPLIED_RESPONSE_MESSAGE =
+        "Invalid username/password supplied";
     private final UserService userService;
-    private final ModelMapper modelMapper;
 
     @PostMapping("/signin")
     @ApiOperation(value = "${UserController.signin}", response = User.class)
     @ApiResponses(value = {
-            @ApiResponse(code = 203, message = "You should change password"),
-            @ApiResponse(code = 400, message = "Something went wrong"),
-            @ApiResponse(code = 422, message = "Invalid username/password supplied")})
-    public User login(@ApiParam("UserName and Password") @RequestBody final SignInRequestDTO signInRequestDTO, final HttpServletResponse res) {
+        @ApiResponse(code = 203, message = YOU_SHOULD_CHANGE_PASSWORD_RESPONSE_MESSAGE),
+        @ApiResponse(code = 400, message = SOMETHING_WENT_WRONG_RESPONSE_MESSAGE),
+        @ApiResponse(code = 422, message = INVALID_USERNAME_PASSWORD_SUPPLIED_RESPONSE_MESSAGE)})
+    public User login(@ApiParam("UserName and Password") @RequestBody final SignInRequestDTO signInRequestDTO,
+                      final HttpServletResponse res) {
         return userService.signIn(signInRequestDTO, res);
     }
 
     @PostMapping("/signup")
     @ApiOperation(value = "${UserController.signup}", response = User.class)
     @ApiResponses(value = {
-            @ApiResponse(code = 400, message = "Something went wrong"),
-            @ApiResponse(code = 403, message = "Access denied"),
-            @ApiResponse(code = 422, message = "Username is already in use"),
-            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+        @ApiResponse(code = 400, message = SOMETHING_WENT_WRONG_RESPONSE_MESSAGE),
+        @ApiResponse(code = 403, message = ACCESS_DENIED_RESPONSE_MESSAGE),
+        @ApiResponse(code = 422, message = USERNAME_IS_ALREADY_IN_USE_RESPONSE_MESSAGE),
+        @ApiResponse(code = 500, message = EXPIRED_OR_INVALID_JWT_TOKEN_MESSAGE)})
     public User signUp(@ApiParam("Signup User") @Valid @RequestBody final User user) {
         return userService.signUp(user, true);
     }
@@ -50,10 +67,10 @@ public class UserController {
     @ApiOperation(value = "${UserController.create}", response = User.class)
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @ApiResponses(value = {
-            @ApiResponse(code = 400, message = "Something went wrong"),
-            @ApiResponse(code = 403, message = "Access denied"),
-            @ApiResponse(code = 422, message = "Username is already in use"),
-            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+        @ApiResponse(code = 400, message = SOMETHING_WENT_WRONG_RESPONSE_MESSAGE),
+        @ApiResponse(code = 403, message = ACCESS_DENIED_RESPONSE_MESSAGE),
+        @ApiResponse(code = 422, message = USERNAME_IS_ALREADY_IN_USE_RESPONSE_MESSAGE),
+        @ApiResponse(code = 500, message = EXPIRED_OR_INVALID_JWT_TOKEN_MESSAGE)})
     public User create(@ApiParam("Create User manually by admin") @RequestBody final User user) {
         return userService.signUp(user, false);
     }
@@ -61,10 +78,10 @@ public class UserController {
     @PutMapping("/update_pass")
     @ApiOperation(value = "${UserController.update_pass}", response = User.class)
     @ApiResponses(value = {
-            @ApiResponse(code = 400, message = "Something went wrong"),
-            @ApiResponse(code = 403, message = "Access denied"),
-            @ApiResponse(code = 422, message = "Username is already in use"),
-            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+        @ApiResponse(code = 400, message = SOMETHING_WENT_WRONG_RESPONSE_MESSAGE),
+        @ApiResponse(code = 403, message = ACCESS_DENIED_RESPONSE_MESSAGE),
+        @ApiResponse(code = 422, message = USERNAME_IS_ALREADY_IN_USE_RESPONSE_MESSAGE),
+        @ApiResponse(code = 500, message = EXPIRED_OR_INVALID_JWT_TOKEN_MESSAGE)})
     public User updatePassword(@ApiParam("Password") @Valid @RequestBody final PasswordUpdateRequestDto user) {
         return userService.updatePassword(user);
     }
@@ -73,10 +90,10 @@ public class UserController {
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @ApiOperation(value = "${UserController.delete}")
     @ApiResponses(value = {
-            @ApiResponse(code = 400, message = "Something went wrong"),
-            @ApiResponse(code = 403, message = "Access denied"),
-            @ApiResponse(code = 404, message = "The user doesn't exist"),
-            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+        @ApiResponse(code = 400, message = SOMETHING_WENT_WRONG_RESPONSE_MESSAGE),
+        @ApiResponse(code = 403, message = ACCESS_DENIED_RESPONSE_MESSAGE),
+        @ApiResponse(code = 404, message = "The user doesn't exist"),
+        @ApiResponse(code = 500, message = EXPIRED_OR_INVALID_JWT_TOKEN_MESSAGE)})
     public String delete(@ApiParam("Username") @PathVariable final String username) {
         userService.delete(username);
         return username;
@@ -86,10 +103,10 @@ public class UserController {
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @ApiOperation(value = "${UserController.search}", response = User.class)
     @ApiResponses(value = {
-            @ApiResponse(code = 400, message = "Something went wrong"),
-            @ApiResponse(code = 403, message = "Access denied"),
-            @ApiResponse(code = 404, message = "The user doesn't exist"),
-            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+        @ApiResponse(code = 400, message = SOMETHING_WENT_WRONG_RESPONSE_MESSAGE),
+        @ApiResponse(code = 403, message = ACCESS_DENIED_RESPONSE_MESSAGE),
+        @ApiResponse(code = 404, message = "The user doesn't exist"),
+        @ApiResponse(code = 500, message = EXPIRED_OR_INVALID_JWT_TOKEN_MESSAGE)})
     public User search(@ApiParam("Username") @PathVariable final String username) {
         return userService.search(username);
     }
@@ -97,9 +114,9 @@ public class UserController {
     @GetMapping(value = "/me")
     @ApiOperation(value = "${UserController.me}", response = User.class)
     @ApiResponses(value = {
-            @ApiResponse(code = 400, message = "Something went wrong"),
-            @ApiResponse(code = 403, message = "Access denied"),
-            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+        @ApiResponse(code = 400, message = SOMETHING_WENT_WRONG_RESPONSE_MESSAGE),
+        @ApiResponse(code = 403, message = ACCESS_DENIED_RESPONSE_MESSAGE),
+        @ApiResponse(code = 500, message = EXPIRED_OR_INVALID_JWT_TOKEN_MESSAGE)})
     public User findMe(final HttpServletRequest req) {
         return userService.findMe(req);
     }
@@ -107,9 +124,9 @@ public class UserController {
     @GetMapping(value = "/logout")
     @ApiOperation(value = "${UserController.logout}")
     @ApiResponses(value = {
-            @ApiResponse(code = 400, message = "Something went wrong"),
-            @ApiResponse(code = 403, message = "Access denied"),
-            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+        @ApiResponse(code = 400, message = SOMETHING_WENT_WRONG_RESPONSE_MESSAGE),
+        @ApiResponse(code = 403, message = ACCESS_DENIED_RESPONSE_MESSAGE),
+        @ApiResponse(code = 500, message = EXPIRED_OR_INVALID_JWT_TOKEN_MESSAGE)})
     public void logOut(final HttpServletRequest req) {
         userService.logOut(req);
     }
@@ -117,10 +134,11 @@ public class UserController {
     @PutMapping(value = "/update_pass/without_previous")
     @ApiOperation(value = "${UserController.update_pass_without_previous}")
     @ApiResponses(value = {
-            @ApiResponse(code = 400, message = "The user with this email doesn't exist oor you didn't activate account"),
-            @ApiResponse(code = 403, message = "Access denied"),
-            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
-    public ResponseEntity<User> updatePassword(@ApiParam("Update Password") @Valid @RequestBody final ForgotPasswordRequestDto forgotPasswordRequestDto) {
+        @ApiResponse(code = 400, message = "The user with this email doesn't exist oor you didn't activate account"),
+        @ApiResponse(code = 403, message = ACCESS_DENIED_RESPONSE_MESSAGE),
+        @ApiResponse(code = 500, message = EXPIRED_OR_INVALID_JWT_TOKEN_MESSAGE)})
+    public ResponseEntity<User> updatePassword(@ApiParam(
+        "Update Password") @Valid @RequestBody final ForgotPasswordRequestDto forgotPasswordRequestDto) {
         return userService.updatePassword(forgotPasswordRequestDto);
     }
 }
